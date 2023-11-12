ef6c39c1a03c38fa34879b94942c8b396b960648
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-11-12 17:17:39 +0100

    refactoring code

--- script.js
+++ script.js
@@ -5,21 +5,27 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', () => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', () => {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
 
-document.addEventListener('keydown', (e) => {
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
   if (e.key == 'Escape') {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
   }
 });
